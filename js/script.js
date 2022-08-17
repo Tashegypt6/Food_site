b73d55835365bcea100bffd9eaa1b048483c0561
Tash gaaba <tashegypt6@yahoo.com>
2022-08-17 14:56:27 +0200

    start new repo

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active')
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active')
+}
+
+let s_icon = document.querySelector('#search-icon');
+let s_form = document.querySelector('#search-form');
+
+s_icon.onclick = () =>{
+    s_form.classList.toggle('active');
+};
+
+let closeI = document.querySelector('#close');
+
+
+closeI.onclick = () =>{
+    s_form.classList.remove('active');
+};
